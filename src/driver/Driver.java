4ca8310b69293273628330a79a7c8a0e87f66c9e
Tan Kucukoglu <tan_kucukoglu@hotmail.com>
2015-03-16 21:59:01 +0200

    App window

---
+++ src/driver/Driver.java
@@ -0,0 +1,43 @@
+package driver;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Driver {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Driver window = new Driver();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Driver() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 640, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
