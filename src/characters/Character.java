18c5ef3099cbdc442f1bf758b8de74eb842bc160
Tan Kucukoglu <tan.kucukoglu@gmail.com>
2015-03-17 23:15:27 +0200

    Character file

---
+++ src/characters/Character.java
@@ -0,0 +1,15 @@
+package characters;
+
+import elements.Element;
+
+public class Character{
+	private String name;
+	private String gender;
+	
+	private Element element;
+	
+	public Character(){
+		
+	}
+	
+}
