1019b815a46380d4cc008bd41681b327e0034cba
Tan Kucukoglu <tan.kucukoglu@gmail.com>
2015-03-18 10:03:59 +0200

    Components File

--- src/characters/Character.java
+++ src/characters/Character.java
@@ -1,10 +1,14 @@
 package characters;
 
+import javax.swing.ImageIcon;
+
 import elements.Element;
 
 public class Character{
 	private String name;
 	private String gender;
+	
+	private ImageIcon icon; // maybe?
 	
 	private Element element;
 	
--- src/elements/Element.java
+++ src/elements/Element.java
@@ -1,10 +1,15 @@
 package elements;
 
+import java.util.ArrayList;
+
+import skills.Skill;
+
 public class Element{
 	
 	private String name; // air, earth, fire, lightning, water
 	private String description;
 	
+	private ArrayList<Skill> skillSet;	
 	
 	public Element(){
 		
