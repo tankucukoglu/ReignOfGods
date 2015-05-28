760ef944ebb351625026ba94b5f70a49f4c0b5a4
Tan Kucukoglu <tan.kucukoglu@gmail.com>
2015-05-28 22:41:18 +0300

    Trying lwjgl and slick2d

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,24 +1,132 @@
 package driver;
 
-import java.awt.BorderLayout;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+//import java.awt.BorderLayout;
+//import java.awt.Graphics;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.JFrame;
+//import javax.swing.JLabel;
+//
+//import components.Skill;
+//import database.SkillCollection;
+//import components.Item;
+//import database.ItemCollection;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-import components.Skill;
-import database.SkillCollection;
-import components.Item;
-import database.ItemCollection;
+import org.lwjgl.Sys;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+ 
+import java.nio.ByteBuffer;
+ 
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Driver{
+	
+	// We need to strongly reference callback instances.
+    private GLFWErrorCallback errorCallback;
+    private GLFWKeyCallback   keyCallback;
+ 
+    // The window handle
+    private long window;
+ 
+    public void run() {
+        System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+ 
+        try {
+            init();
+            loop();
+ 
+            // Release window and window callbacks
+            glfwDestroyWindow(window);
+            keyCallback.release();
+        } finally {
+            // Terminate GLFW and release the GLFWerrorfun
+            glfwTerminate();
+            errorCallback.release();
+        }
+    }
+ 
+    private void init() {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+ 
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( glfwInit() != GL11.GL_TRUE )
+            throw new IllegalStateException("Unable to initialize GLFW");
+ 
+        // Configure our window
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
+ 
+        int WIDTH = 300;
+        int HEIGHT = 300;
+ 
+        // Create the window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+ 
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
+            }
+        });
+ 
+        // Get the resolution of the primary monitor
+        ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        // Center our window
+        glfwSetWindowPos(
+            window,
+            (GLFWvidmode.width(vidmode) - WIDTH) / 2,
+            (GLFWvidmode.height(vidmode) - HEIGHT) / 2
+        );
+ 
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+ 
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+ 
+    private void loop() {
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the ContextCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GLContext.createFromCurrent();
+ 
+        // Set the clear color
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+ 
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+ 
+            glfwSwapBuffers(window); // swap the color buffers
+ 
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+        }
+    }
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 		
 		// testing skills and items files
 		
@@ -36,22 +144,24 @@ public class Driver{
 //			System.out.println(test.get(i));
 //		}
 		
-		final BufferedImage img = ImageIO.read(new File("database/images/map.png"));
+//		final BufferedImage img = ImageIO.read(new File("database/images/map.png"));
+//		
+//		JFrame frame = new JFrame("RPG");
+//		JLabel label = new JLabel(){
+//			@Override
+//            protected void paintComponent(Graphics g){
+//                super.paintComponent(g);
+//                g.drawImage(img, 0, 0, null);
+//            }
+//		};
+//
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		
+//		frame.getContentPane().add(label, BorderLayout.CENTER);
+//		frame.setSize(640, 480);
+//		//frame.pack(); //resize window according to frame contents
+//		frame.setVisible(true);
 		
-		JFrame frame = new JFrame("RPG");
-		JLabel label = new JLabel(){
-			@Override
-            protected void paintComponent(Graphics g){
-                super.paintComponent(g);
-                g.drawImage(img, 0, 0, null);
-            }
-		};
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.getContentPane().add(label, BorderLayout.CENTER);
-		frame.setSize(640, 480);
-		//frame.pack(); //resize window according to frame contents
-		frame.setVisible(true);
+		new Driver().run();
 	}
 }
