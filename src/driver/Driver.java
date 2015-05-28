235c5c155550180f000ae68c15f1bf6649bfe973
Tan Kucukoglu <tan.kucukoglu@gmail.com>
2015-05-28 22:03:56 +0300

    State machine and image test

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,6 +1,15 @@
 package driver;
 
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import components.Skill;
 import database.SkillCollection;
@@ -9,22 +18,40 @@ import database.ItemCollection;
 
 public class Driver{
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		
 		// testing skills and items files
 		
-		SkillCollection skills = new SkillCollection();
-		ItemCollection items = new ItemCollection();
+//		SkillCollection skills = new SkillCollection();
+//		ItemCollection items = new ItemCollection();
+//		
+//		ArrayList<Skill> test = skills.getSkills();
+//		ArrayList<Item> test2 = items.getItems();
+//		
+//		for(int i = 0; i < test2.size(); i++){
+//			System.out.println(test2.get(i));
+//		}
+//		System.out.println();
+//		for(int i = 0; i < test.size(); i++){
+//			System.out.println(test.get(i));
+//		}
 		
-		ArrayList<Skill> test = skills.getSkills();
-		ArrayList<Item> test2 = items.getItems();
+		final BufferedImage img = ImageIO.read(new File("database/images/map.png"));
 		
-		for(int i = 0; i < test2.size(); i++){
-			System.out.println(test2.get(i));
-		}
-		System.out.println();
-		for(int i = 0; i < test.size(); i++){
-			System.out.println(test.get(i));
-		}
+		JFrame frame = new JFrame("RPG");
+		JLabel label = new JLabel(){
+			@Override
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                g.drawImage(img, 0, 0, null);
+            }
+		};
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+		frame.setSize(640, 480);
+		//frame.pack(); //resize window according to frame contents
+		frame.setVisible(true);
 	}
 }
